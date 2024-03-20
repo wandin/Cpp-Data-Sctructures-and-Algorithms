9614d86dbe8f144821c2c6d0e41e4d26f48a1344
wandin <wanderson.lopes@outlook.com>
2024-03-19 23:43:16 -0300

    Doubly Linked List final implementation

--- LinkedLists/DoublyLinkedList.cpp
+++ LinkedLists/DoublyLinkedList.cpp
@@ -43,9 +43,7 @@ public:
 	int Search(T);				// params - element to search, returns its position
 	int Length();				// returns the length of the linked list.
 	bool isEmpty();				// check if it's empty and returns true or false 
-	bool isFull();				// check if it's full and returns true or false
-	void Display();				// display all the elements in the list
-		
+	void Display();				// display all the elements in the list	
 		
 };
 
@@ -62,61 +60,154 @@ DoublyLinkedList<T>::DoublyLinkedList()
 template <class T>
 DoublyLinkedList<T>::~DoublyLinkedList()
 {
-	
+	Node<T> *p = first;
+	while(p)
+	{
+		first = first->next;
+		delete p;
+		p = first;
+	}
 }
 
 template <class T>
 void DoublyLinkedList<T>::Insert(int pos, T Element)
 {
-	Node<T> *temp;
-	temp = new Node<T>;
-	temp->Data = Element;
-	temp->prev = NULL;
-	temp->next = NULL;
+    Node<T> *temp;
+    temp = new Node<T>; // Create a new node
+    temp->Data = Element; // Assign data to the new node
+    temp->prev = NULL;
+    temp->next = NULL;
 	
-	
-	// if is the first position
-	// if first node is not valid, so the list is empty. We'll be inserting in the first node then.
-	
-	if(length == 0) 
-	{
-		first = temp;
-		last = temp;
-	}
-	else 
-	{
-		if(pos == 1) // if is first position
-		{
-			temp->next = first;
-			first->prev =  temp;
-			first = temp;	
-		}
-		else if (pos == length +1) // if last position
-		{
-			temp->prev = last;
-			last->next = temp;
-			last = temp;
-		}
-		else	// middle position
-		{
-			Node<T> *p = first;
-			
-			for (int i = 1; i <= pos-2; i++)
-			{
-				p = p->next;
-			}
-			temp->next = p->next;
-			temp->prev = p;
-			p->next->prev = temp;
-			p->next = temp;
-		}
-	}
-	length++;
+    // If the list is empty
+    if(length == 0) 
+    {
+        first = temp; // Set the new node as both first and last node
+        last = temp;
+    }
+    else 
+    {
+        // If inserting at the beginning
+        if(pos == 1) 
+        {
+            temp->next = first; 	// Set the next of the new node to the current first node
+            first->prev =  temp; 	// Set the previous of the current first node to the new node
+            first = temp; 			// Update the first pointer to the new node	
+        }
+        // If inserting at the end
+        else if (pos == length +1) 
+        {
+            temp->prev = last; 		// Set the previous of the new node to the current last node
+            last->next = temp; 		// Set the next of the current last node to the new node
+            last = temp; 			// Update the last pointer to the new node
+        }
+        else	// Inserting in the middle
+        {
+            Node<T> *p = first;
+            // Traverse to the node before the position to be inserted
+            for (int i = 1; i <= pos-2; i++)
+            {
+                p = p->next;
+            }
+            temp->next = p->next; 	// Set the next of the new node to the next of the current node
+            temp->prev = p; 		// Set the previous of the new node to the current node
+            p->next->prev = temp; 	// Set the previous of the next node to the new node
+            p->next = temp; 		// Set the next of the current node to the new node
+        }
+    }
+    length++; // Increase the length of the list
 }
 
 
 
+/* DELETE FUNCTION */
+template<class T>
+void DoublyLinkedList<T>::Delete(int pos, T& Element)
+{
+	Node<T> *p, *q;
+	
+	if(length == 0)
+	{
+		cout<<"Nothing to delete - List is empty"<<endl;
+	}
+	if(pos == 1)
+	{
+        p = first; 				// Store the first node
+        first = first->next; 	// Update the first pointer to the next node
+        first->prev = NULL; 	// Set the previous of the new first node to NULL
+        Element = p->Data; 		// Store the data of the deleted node
+        delete p; 				// Delete the node
+	}
+	else if(pos == length)
+	{
+        p = last; 				// Store the last node
+        last = last->prev; 		// Update the last pointer to the previous node
+        last->next = NULL; 		// Set the next of the new last node to NULL
+        Element = p->Data; 		// Store the data of the deleted node
+        delete p; 				// Delete the node
+	}
+	else
+	{
+        p = first; // Start from the first node
+        for (int i = 1; i <= pos-2; i++) // Traverse to the node before the position to be deleted
+        {
+            p = p->next;
+        }
+        q = p->next; 			// Store the node to be deleted
+        q->next->prev = p; 		// Update the previous pointer of the next node
+        Element = q->Data; 		// Store the data of the deleted node
+        delete q; 				// Delete the node
+	}
+	length--;					// Decrease the length of the list
+}
 
+template<class T> 
+bool DoublyLinkedList<T>::Find(int pos, T& Element)
+{
+   	Node<T> *p = first;
+	
+	if(pos < 1 || pos > length) // if position is before first, or after last, doesn`t exist
+	{
+		return false;
+	}
+	// else we loop through
+	for (int i = 1; i < pos; i++)
+	{
+		p = p->next;
+	}
+	Element = p->Data;
+	return true;
+}
+
+template <class T>
+int DoublyLinkedList<T>::Search(T Element)
+{
+	Node<T> *p = first;
+	int index = 1;
+	while(p)
+	{
+		if(p->Data == Element)
+		{
+			return index;
+		}
+		p = p->next;
+		index++;
+	}
+	return -1;	
+}
+
+/* LENGTH */
+template <class T>
+int DoublyLinkedList<T>::Length()
+{
+	return length;
+}
+
+/* is Empty */
+template <class T>
+bool DoublyLinkedList<T>::isEmpty()
+{
+	return first == NULL;
+}
 
 
 /* DISPLAY */
@@ -136,16 +227,53 @@ main()
 {
 	DoublyLinkedList<int> DLL;
 	
-	
-	DLL.Insert(1, 10);
-	DLL.Insert(2, 20);
-	DLL.Insert(3, 30);
-	DLL.Insert(4, 40);
-	DLL.Insert(5, 50);
-	DLL.Insert(6, 60);
-
-	
+	/* Insert Method */
+	cout<<"\nINSERT"<<endl;
+	DLL.Insert(1, 100);
+	DLL.Insert(2, 200);
+	DLL.Insert(3, 300);
+	DLL.Insert(4, 400);
+	DLL.Insert(5, 500);
+	DLL.Insert(6, 600);
 	DLL.Display();
+	cout<<"\n---------------------------"<<endl;
+		
+	/* Delete Method*/
+	cout<<"\nDELETE"<<endl;
+	int deleted;
+	DLL.Delete(6,deleted);
+	DLL.Display();
+	cout<<"\n---------------------------"<<endl;
+	
+	/* FIND Method*/
+	cout<<"\nFIND"<<endl;
+	int find;
+	if(DLL.Find(4, find))
+	{
+		cout <<"Element found: "<<find<<endl;
+	}
+	cout<<"\n---------------------------"<<endl;
+
+	// EMPTY
+	cout<<"\nEMPTY"<<endl;
+	if(DLL.isEmpty())
+        cout<<"\nList is EMPTY...\n";
+    else
+        cout<<"\nList is NOT EMPTY...\n";
+	cout<<"\n---------------------------"<<endl;
+
+
+	/* SEARCH Method*/
+	cout<<"\nSEARCH"<<endl;
+	int searched = DLL.Search(300);
+	cout<<"\nElement Found at Position "<<searched<<endl;
+	cout<<"\n---------------------------"<<endl;
+
+	// RETURN LENGTH
+	cout<<"\nLENGTH"<<endl;
+    cout<<"\nThe Length of the List is..."<<DLL.Length();
+	
+
 	
 	return 0;
 };
